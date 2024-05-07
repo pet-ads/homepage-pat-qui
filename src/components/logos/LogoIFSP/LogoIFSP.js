ae12aff7386735cb2015cc21c76909ea3fdb5464
Vitor5bonelli <bonelli5.vitor@gmail.com>
2024-05-07 15:28:36 -0300

    fix: logo redirects to IFSP/SRT

--- src/components/logos/LogoIFSP/LogoIFSP.js
+++ src/components/logos/LogoIFSP/LogoIFSP.js
@@ -2,7 +2,7 @@ import styles from './LogoIFSP.module.css'
 import IFSPLogo from '../../../resources/logos/LogoSRT.png'
 function LogoIFSP (){
     return(
-        <a href='https://scl.ifsp.edu.br' rel="noreferrer" target="_blank">
+        <a href='https://srt.ifsp.edu.br/' rel="noreferrer" target="_blank">
                  <img className={styles.IFSPLogo} src={IFSPLogo}  alt='Identidade visual IFSP/SCL'/>  
         </a>
 
