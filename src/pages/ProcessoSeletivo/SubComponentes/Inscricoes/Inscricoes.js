5b3e8248012d4bee2bdef653cfbd02f2caa6185f
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-07-09 09:49:55 -0300

    feat: add dynamic Gmail link (mailto) with subject and body based on state validation using useEffect, and conditional rendering for registration deadline

--- src/pages/ProcessoSeletivo/SubComponentes/Inscricoes/Inscricoes.js
+++ src/pages/ProcessoSeletivo/SubComponentes/Inscricoes/Inscricoes.js
@@ -1,4 +1,5 @@
 import { AiOutlineCloudDownload } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
 import styles from "./Inscricoes.module.css";
 
 function Inscricoes({
@@ -10,6 +11,27 @@ function Inscricoes({
   estado,
   texto_3,
 }) {
+  const [subject, setSubject] = useState("");
+  const [body, setBody] = useState("");
+
+  useEffect(() => {
+    if (estado.toLowerCase() !== "encerrado") {
+      setSubject(
+        "Inscrição no Processo Seletivo PET Química - IFSP Campus Sertãozinho"
+      );
+      setBody(
+        "Olá,%0D%0A%0D%0AGostaria de obter mais informações sobre o edital do processo seletivo do PET Química do IFSP Campus Sertãozinho. Estou interessado em participar do edital vigente do programa e gostaria de esclarecer algumas dúvidas que não estão disponíveis no site sobre os documentos necessários, critérios de seleção, cronograma e outras informações relevantes.%0D%0A%0D%0AAgradeço antecipadamente pela sua atenção e aguardo seu retorno.%0D%0A%0D%0AAtenciosamente,%0D%0A%0D%0A[Seu Nome]%0D%0A[Seu E-mail]"
+      );
+    } else {
+      setSubject(
+        "Informações sobre futuros editais do PET Química - IFSP Campus Sertãozinho"
+      );
+      setBody(
+        "Olá,%0D%0A%0D%0AGostaria de obter mais informações sobre quando serão publicados novos editais para o processo seletivo do PET Química do IFSP Campus Sertãozinho. Estou interessado em participar do programa futuramente e gostaria de esclarecer algumas dúvidas que não estão disponíveis no site sobre os documentos necessários, critérios de seleção, cronograma e outras informações relevantes.%0D%0A%0D%0AAgradeço antecipadamente pela sua atenção e aguardo seu retorno.%0D%0A%0D%0AAtenciosamente,%0D%0A%0D%0A[Seu Nome]%0D%0A[Seu E-mail]"
+      );
+    }
+  }, [estado]);
+
   return (
     <div className={styles.Inscricao}>
       <h2 className={styles.titulo}>{titulo}</h2>
@@ -17,7 +39,7 @@ function Inscricoes({
         {texto_1}{" "}
         <a
           className={styles.email}
-          href={`mailto:${email}`}
+          href={`mailto:${email}?subject=${subject}&body=${body}`}
           target="_blank"
           rel="noreferrer"
         >
@@ -25,9 +47,15 @@ function Inscricoes({
         </a>
         .
       </p>
-      <p className={styles.texto}>
-        {texto_2} <span className={styles.encerrado}>{data}</span> {estado}
-      </p>
+      {estado.toLowerCase() === "encerrado" ? (
+        <p className={styles.texto}>
+          {texto_2} <span className={styles.encerrado}>{data}</span> ({estado}.)
+        </p>
+      ) : (
+        <p className={styles.texto}>
+          {texto_2} {data} ({estado}).
+        </p>
+      )}
       <p className={styles.texto}>{texto_3} </p>
       <p className={styles.download}>
         <a href="a" rel="noreferrer" target="_blank">
